9266146f1c42e4af484d7c247225b6e7e7a0bbd8
juanmnl <juanmnlcornejo@gmail.com>
2016-02-01 20:35:18 -0500

    Add randomness to bg, wrap bg script as self invoking fn

--- scripts.js
+++ scripts.js
@@ -1,49 +1,51 @@
-'use strict';
+(function() {
+  'use strict';
 
-var c = document.getElementsByTagName('canvas')[0],
-x = c.getContext('2d'),
-pr = window.devicePixelRatio || 1,
-w = window.innerWidth,
-h = window.innerHeight,
-f = w,
-q,
-m = Math,
-r = 10,
-u = m.PI*2,
-v = m.cos,
-z = m.random;
-c.width = w*pr;
-c.height = h*pr;
-x.scale(pr, pr);
-x.globalAlpha = 0.1;
+  var c = document.getElementsByTagName('canvas')[0],
+  x = c.getContext('2d'),
+  pr = window.devicePixelRatio || 1,
+  w = window.innerWidth,
+  h = window.innerHeight,
+  f = w,
+  q,
+  m = Math,
+  r = 1,
+  u = m.random,
+  v = m.random,
+  z = m.random;
+  c.width = w*pr;
+  c.height = h*pr;
+  x.scale(pr, pr);
+  x.globalAlpha = 0.1;
 
-function i() {
-  x.clearRect(0,0,w,h);
-  q=[{x:0,y:h*.7+f},{x:0,y:h*.7-f}];
-  while(q[1].x<w+f) d(q[0], q[1]);
-}
+  function init() {
+    x.clearRect(0,0,w,h);
+    q=[{x:0,y:h*.5+f},{x:0,y:h*.5-f}];
+    while(q[1].x<w+f) d(q[0], q[1]);
+  }
 
-function d(i,j) {
-  x.beginPath();
-  x.moveTo(i.x, i.y);
-  x.lineTo(j.x, j.y);
-  var k = j.x + (z()*2-0.25)*f,
-  n = y(j.y);
-  x.lineTo(k, n);
-  x.closePath();
-  r-=u/-50;
-  x.fillStyle = '#'+(v(r)*128+127<<16 | v(r+u/3)*128+127<<8 | v(r+u/3*2)*128+127).toString(16);
-  x.fill();
-  q[0] = q[1];
-  q[1] = {x:k,y:n};
-}
+  function d(init,j) {
+    x.beginPath();
+    x.moveTo(init.x, init.y);
+    x.lineTo(j.x, j.y);
+    var k = j.x + (z()*2-0.25)*f,
+    n = y(j.y);
+    x.lineTo(k, n);
+    x.closePath();
+    r-=u/-50;
+    x.fillStyle = '#'+(v(r)*128+127<<16 | v(r+u/3)*128+127<<8 | v(r+u/3*2)*128+127).toString(16);
+    x.fill();
+    q[0] = q[1];
+    q[1] = {x:k,y:n};
+  }
 
-function y(p) {
-  var t = p + (z()*2-1.1)*f;
-  return (t>h||t<0) ? y(p) : t;
-}
+  function y(p) {
+    var t = p + (z()*2-1.1)*f;
+    return (t>h||t<0) ? y(p) : t;
+  }
 
-document.onclick = i;
-document.ontouchstart = i;
+  document.onclick = init;
+  document.ontouchstart = init;
 
-i();
+  init();
+})();
